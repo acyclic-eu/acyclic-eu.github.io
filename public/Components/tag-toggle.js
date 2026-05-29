2904c5b99b418c2cb92db219f1c4d2b74ab5f66c
Patrik Gustafsson <patrik@acyclic.eu>
2026-05-29 15:05:42 +0200

    Fix: Prevent tag-toggle flicker on hover by adding transparent border

--- public/Components/tag-toggle.js
+++ public/Components/tag-toggle.js
@@ -42,6 +42,7 @@ export default define({
       .toggle-label {
         padding: 0.3em 0.5em;
         border-radius: 3px;
+        border: 1px solid transparent;
         transition: all 0.2s ease;
         /*noinspection CssUnresolvedCustomProperty*/
         background-color: var(--tag-toggle-unselected-bg);
